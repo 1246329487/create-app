179564e848b1fd86209184bf400a51ddb5945500
yunxi <1246329487@qq.com>
2023-05-22 22:28:08 +0800

    引入ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
+/*
+ * @Author: yunxi 1246329487@qq.com
+ * @Date: 2023-05-22 17:14:52
+ * @LastEditors: yunxi 1246329487@qq.com
+ * @LastEditTime: 2023-05-22 17:28:53
+ * @FilePath: \my-app\src\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+//@ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +22,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
